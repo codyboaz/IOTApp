1ea9671018c4c724753505ab4b7e26bb15012d2a
Cody Boaz <cboaz@csumb.edu>
2016-12-13 20:05:37 -0800

    12/13/16

--- app/views/credentials/credentials.js
+++ app/views/credentials/credentials.js
@@ -2,23 +2,31 @@ var frameModule = require("ui/frame");
 var textFieldModule = require("ui/text-field");
 var dialogs = require("ui/dialogs");
 
-
+        
+console.log("loading splash");
 exports.pageLoaded = function(args) {
     var page = args.object;
     page.bindingContext = {};
-
+    global.estimoteMaster.running.startRanging();
+	
 }
 
 exports.homepage = function() {
-    frameModule.topmost().navigate("views/home/home");
+	console.log("loading home");
+	var navigationOptions = {
+        moduleName: 'views/home/home',
+        context: {}
+    }
+    frameModule.topmost().navigate(navigationOptions);
 }
 
 exports.instructions = function() {
-  dialogs.alert({
-  title: "Instructions",
-  message: "IMPORTANT: Please make sure the bluetooth on your phone is turned on. We have iBeacons set up around the building. If you are on the Event Map or Schedule pages and are within range of an iBeacon, you will be sent a notification asking if you would like to see that rooms schedule. Try and find all three iBeacons. On the back of the name tags we have given you is a secret identifier. If you know your friends secret identifier you can enter it in the Find Your Friends portion of our app and track their last known location. Have fun and enjoy the 2016 Capstone Festival.",
-  okButtonText: "OK"
-}).then(function () {
-  console.log("Dialog closed!");
-});
+	console.log("dialog box");
+	dialogs.alert({
+		title: "Instructions",
+		message: "IMPORTANT: Please make sure the bluetooth on your phone is turned on. We have iBeacons set up around the building. If you are within range of an iBeacon, you will be sent a notification asking if you would like to see that rooms schedule. Try and find all three iBeacons. On the back of the name tags we have given you is a secret identifier. If you know your friends secret identifier you can enter it in the Find Your Friends portion of our app and track their last known location. Have fun and enjoy the 2016 Capstone Festival.",
+		okButtonText: "OK"
+	}).then(function () {
+		console.log("Dialog closed!");
+	});
 }
--- platforms/ios/build/Pods.build/Debug-iphoneos/PocketSocket.build/DerivedSources/PocketSocket_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char PocketSocketVersionString[];
- extern const double PocketSocketVersionNumber;
-
- const unsigned char PocketSocketVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:PocketSocket  PROJECT:Pods-1" "\n";
- const double PocketSocketVersionNumber __attribute__ ((used)) = (double)1.;
